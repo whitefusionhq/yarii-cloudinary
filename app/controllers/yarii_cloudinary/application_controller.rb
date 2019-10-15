662549b6bab9e4c4f6d528f556196f223c09a27f
Jared White <jared@jaredwhite.com>
2019-10-15 19:08:16 +0000

    Add controller authorization

--- app/controllers/yarii_cloudinary/application_controller.rb
+++ app/controllers/yarii_cloudinary/application_controller.rb
@@ -1,5 +1,6 @@
 module YariiCloudinary
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    include YariiEditor::ControllerAuthorization
   end
 end
--- app/controllers/yarii_cloudinary/media_controller.rb
+++ app/controllers/yarii_cloudinary/media_controller.rb
@@ -1,30 +1,32 @@
-class YariiCloudinary::MediaController < ApplicationController
-  def index
-    json_images = Cloudinary::Api.resources_by_tag('yarii_editor', cloud_name: ENV['yarii_cloudinary_cloud_name'], api_key: ENV['yarii_cloudinary_api_key'], api_secret: ENV['yarii_cloudinary_api_secret'], max_results: 200)['resources']
+module YariiCloudinary
+  class MediaController < ApplicationController
+    def index
+      json_images = Cloudinary::Api.resources_by_tag('yarii_editor', cloud_name: ENV['yarii_cloudinary_cloud_name'], api_key: ENV['yarii_cloudinary_api_key'], api_secret: ENV['yarii_cloudinary_api_secret'], max_results: 200)['resources']
 
-    @images = json_images.map do |image|
-      {
-        path: "v#{image['version']}/#{image['public_id']}",
-        id: image['public_id'],
-        format: image['format']
-      }
+      @images = json_images.map do |image|
+        {
+          path: "v#{image['version']}/#{image['public_id']}",
+          id: image['public_id'],
+          format: image['format']
+        }
+      end
+
+      render layout: false
     end
 
-    render layout: false
-  end
+    def form_thumbnail
+      render partial: 'form_thumbnail', layout: nil, formats: [:html], locals: {public_id: params[:id]}
+    end
 
-  def form_thumbnail
-    render partial: 'form_thumbnail', layout: nil, formats: [:html], locals: {public_id: params[:id]}
-  end
-
-  def multiple_form_item
-    render_locals = {
-      public_id: params[:id],
-      name: params[:name],
-      index: params[:index],
-      input_classes: params[:input_classes],
-      placeholder:  params[:placeholder]
-    }
-    render partial: 'multiple_form_item', layout: nil, formats: [:html], locals: render_locals
+    def multiple_form_item
+      render_locals = {
+        public_id: params[:id],
+        name: params[:name],
+        index: params[:index],
+        input_classes: params[:input_classes],
+        placeholder:  params[:placeholder]
+      }
+      render partial: 'multiple_form_item', layout: nil, formats: [:html], locals: render_locals
+    end
   end
 end
